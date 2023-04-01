f4f6dd26c1b464d0db3b47f63cc8e7e63d377cb9
Onaefe <onaefe@onasonic.com>
2023-04-01 09:08:25 +0100

    AppUser properties addition and version number update

--- Dto/AppUser.cs
+++ Dto/AppUser.cs
@@ -5,6 +5,9 @@
         public virtual string DisplayName { get; set; }
         public virtual string Email { get; set; }
         public virtual long Id { get; set; }
+        public virtual string Guid { get; set; }
         public virtual List<string> Roles { get; set; }
+        public virtual DateTime ExpiresAt { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
     }
 }
