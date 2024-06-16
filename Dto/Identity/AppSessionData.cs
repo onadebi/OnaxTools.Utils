e6089b033d95445093912120333981c83cc5fad9
Onadebi <onadebi@onaxsys.com>
2024-06-16 10:55:14 -0400

    Email restorations and Identifier update

--- Dto/Identity/AppSessionData.cs
+++ Dto/Identity/AppSessionData.cs
@@ -4,9 +4,14 @@ namespace OnaxTools.Dto.Identity
 {
     public class AppSessionData<T> where T : class
     {
+
         public virtual string MachineName { get; set; } = string.Empty;
         public virtual string IpAddress { get; set; }
         public virtual string SessionId { get; set; }
+        public virtual string Email { get; set; } = string.Empty;
+        /// <summary>
+        /// This property serves as an identifiable or anonymous identifier for an object/user.
+        /// </summary>
         public virtual string Identifier { get; set; } = string.Empty;
         public virtual DateTime LastUpdated { get; set; } = DateTime.UtcNow;
         public T Data { get; set; } = null;
